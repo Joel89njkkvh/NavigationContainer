b3ca09f15a2eea70d443058c7edbf1dd9ec26760
Joel <151809158+Joel89njkkvh@users.noreply.github.com>
2025-07-16 21:46:30 -0600

    tabs

--- screens/profile.js
+++ screens/profile.js
@@ -1,17 +1,15 @@
-import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function Profile({ navigation }) {
+export default function Profile() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Pantalla de Perfil</Text>
 
-      <Pressable style={[styles.button, styles.buttonSettings]} onPress={() => navigation.navigate('Settings')}>
-        <Text style={styles.buttonText}>Ir a Configuración</Text>
-      </Pressable>
+      <View style={styles.iconRow}>
+        <Ionicons name="person-outline" size={28} color="green" />
+        <Text style={styles.title}> Perfil de usuario </Text>
+      </View>
 
-      <Pressable style={[styles.button, styles.buttonHome]} onPress={() => navigation.navigate('Home')}>
-        <Text style={styles.buttonText}>Volver a Home</Text>
-      </Pressable>
     </View>
   );
 }
@@ -24,29 +22,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 20,
   },
+  iconRow: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
   title: {
     fontSize: 22,
     fontWeight: 'bold',
-    marginBottom: 40,
-    textAlign: 'center',
-  },
-  button: {
-    paddingVertical: 12,
-    paddingHorizontal: 30,
-    borderRadius: 8,
-    marginBottom: 20,
-    width: '80%',
-    alignItems: 'center',
-  },
-  buttonSettings: {
-    backgroundColor: '#FF8800', 
-  },
-  buttonHome: {
-    backgroundColor: '#20A745', 
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 16,
-    fontWeight: '600',
+    marginLeft: 10,
+    color: 'green',
   },
 });
